7b4d3dd25f84d8ac2de0635f2cac5eb59d2a6b5d
Muhammad Qamar <146847569+MuhammadQamar99@users.noreply.github.com>
2025-07-12 11:43:51 +0500

    Add files via upload

---
+++ 23_String_Methods.py
@@ -0,0 +1,52 @@
+# Subject : Methods of string in python
+# Author : Muhammad Qamar
+# Date : 11-July-2025
+
+# # .center Method
+# txt = "Banana"
+# x = txt.center(20,'*')
+# print(x)
+
+
+# # .count method
+# a = "My name is Muhammad"
+# print(a.count(' '))
+
+# # .endswith method
+# b = "Muhammad"
+# print(b.endswith('a'))
+
+# # .encode method
+# c = "Muhammad"
+# print(c.encode())
+
+# # .find method
+# d = "muhammad"
+# print(d.find('d'))
+
+# # .expand method
+# e = "h\te\tl\tl\to"
+# print(e.expandtabs(2))
+
+# #  .isalnum method
+# f = "muhammad-123"
+# print(f.isalnum())
+
+# # .isdecimal method
+# g = '2334'
+# print(g.isdecimal())
+
+# # .isdigit method
+# h = '1234'
+# print(h.isdigit())
+
+# # .isprintable method
+# txt = "Hello! Are you #1?"
+# x = txt.isprintable()
+# print(x)
+
+# .join method
+i = {"name":"Muhammad","age":"17"}
+j = "TEST"
+k = j.join(i)
+print(k)
