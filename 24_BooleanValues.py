ecfb54a5ca48ef304f1aa016f9f7eb4da7f3f8c9
Muhammad Qamar <146847569+MuhammadQamar99@users.noreply.github.com>
2025-07-14 11:27:49 +0500

    Add files via upload

---
+++ 24_BooleanValues.py
@@ -0,0 +1,70 @@
+# Subject : Boolean Values in python
+# Author : Muhammad Qamar
+# Date : 12-July-2025
+
+# # =======================================
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+# # ========================================
+# a = 200
+# b = 33
+
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("a is greater than b")
+
+# # =====================================
+# print(bool("hello"))
+# print(bool(15))
+
+# # =====================================
+# x = "hello"
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
+
+# # =====================================
+# print(bool("abc"))
+# print(bool("123"))
+# print(bool("[1,2,3]"))
+
+# # =====================================
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+# print(bool([]))
+# print(bool({}))
+
+# # =====================================
+# class myclass():
+#     def __len__(self):
+#         return 0
+    
+# myobj = myclass()
+# print(bool(myobj))
+
+# # =====================================
+# def function():
+#     return True
+
+# print(function())
+
+# # =====================================
+# def function():
+#     return True
+
+# if function():
+#     print("Yes")
+# else:
+#     print("NO")
+
+# # =====================================
+# x = 200
+# print(isinstance(x , int))
+
---
+++ 25_PythonOperators.py
@@ -0,0 +1,53 @@
+# Subject : Python Operators
+# Author : Muhammad Qamar
+# Date : 12-July-2025
+
+# print(10 + 5)
+
+# # Arithmetic Operators
+# print(10 + 5)
+# print(10 - 5)
+# print(10 * 5)
+# print(10 / 5)
+# print(10 % 5)
+# print(10 ** 5)
+# print(10 // 5)
+
+# # Assignment Operators
+# x = 5
+# print(x)
+
+# x += 3
+# print(x)
+
+# x -= 3
+# print(x)
+
+# x *= 3
+# print(x)
+
+# x /= 3
+# print(x)
+
+# x %= 3
+# print(x)
+
+# x = 10
+# x //= 3
+# print(x)
+
+# x **= 3
+# print(x)
+
+# x = 5
+# x &= 3
+# print(x)
+
+# x = 5
+# x |= 3
+# print(x)
+
+# x = 5
+# x ^= 3
+# print(x)
+
