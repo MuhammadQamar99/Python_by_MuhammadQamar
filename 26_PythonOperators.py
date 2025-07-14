2dc69ba133d16888e822bb3cd1a3250d30027c9a
Muhammad Qamar <146847569+MuhammadQamar99@users.noreply.github.com>
2025-07-14 11:27:21 +0500

    Add files via upload

---
+++ 26_PythonOperators.py
@@ -0,0 +1,44 @@
+# Subject : Pyhton Operators
+# Author : Muhammad Qamar
+# Date : 14-July-2025
+
+# # Comparison Operators
+# print(6 == 6)
+# print(6 > 6)
+# print(3 < 6)
+# print(3 != 6)
+# print( 3 >= 6)
+# print(3 <= 6)
+
+# # Logical Operators
+# x = 7
+# if x < 5 and x < 10:
+#     print("True")
+# else:
+#     print("False")
+
+# y = 10
+# if y < 5 or y < 10:
+#     print("True")
+# else:
+#     print("False")
+
+# z = 1
+# if not(z == 10):
+#     print("True")
+# else:
+#     print("False")
+
+# # identity Operators
+# a = ["apple","banana"]
+# b = ["apple","banana"]
+
+# a = b 
+# print(a is b)
+
+# Membership Operators
+a = ["apple","banana"]
+b = ["apple","banana"]
+
+print("apple" in a)
+print("grapes" in a)
