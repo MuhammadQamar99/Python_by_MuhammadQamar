cd6cb1191c5231c96784699d017a41257799a901
Muhammad Qamar <146847569+MuhammadQamar99@users.noreply.github.com>
2025-07-22 08:48:30 +0500

    Add files via upload

---
+++ 37_Python_Tuples.py
@@ -0,0 +1,36 @@
+# Subject : Python Tuples
+# Author : Muhammad Qamar
+# Date : 19-July-2025
+
+# mytuple = ("apple", "banana", "cherry")
+# print(mytuple)
+
+# # Tuple
+# this_tuple = ('a','b','c','d')
+# print(this_tuple)
+
+# # Tuple Items
+# tuple_1 = ('a','b','c','d','c','b','a')
+# print(tuple_1)
+
+
+# # Tuple Length
+# tuple_1 = ('a','b','c','d','c','b','a')
+# print(len(tuple_1))
+
+
+# # Create Tuple With One Item
+# thistuple = ("apple",)
+# print(type(thistuple))
+
+# #NOT a tuple
+# thistuple = ("apple")
+# print(type(thistuple))
+
+
+# # Tuple Items - Data Types
+# tuple1 = ("abc", 34, True, 40.1, "male")
+# print(tuple1)
+
+fruits = tuple(('APPLE'))
+print(type(fruits))
