537f38378a7eeac074e8768f0ed6714c81a641f7
Muhammad Qamar <146847569+MuhammadQamar99@users.noreply.github.com>
2025-07-23 14:35:33 +0500

    Add files via upload

---
+++ 44_Python_Sets.py
@@ -0,0 +1,39 @@
+# Subject : Python Sets 
+# Author : Muhammad Qamar
+# Date : 23-July-2025
+
+# # Example
+# myset = {"apple", "banana", "cherry"}  # [We use it for list], (We use it for tuples)
+# print(myset)
+# print(type(myset))
+
+# set_1 = {'apple','banana','cherry','mango','orange'}
+# print(set_1)
+# print(type(set_1))
+
+
+
+# # Duplicates are not allowed
+# set_2 = {'apple','banana','cherry','apple','mango','orange','banana'}
+# print(set_2)
+
+
+# # True == 1
+# set_2 = {'apple','banana','cherry','apple','mango','orange','banana',True, 1, 2}
+# print(set_2)
+
+
+# # false == 0
+# set_2 = {'apple','banana','cherry','apple','mango','orange','banana',False, True, 0}
+# print(set_2)
+
+
+
+# # len() function
+# set_2 = {'apple','banana','cherry','apple','mango','orange','banana',False, True, 0}
+# print(len(set_2))
+
+
+# set() constructor
+set_3 = set(('a','b','c','d'))
+print(type(set_3))
