2a47a3b66ec5a9f531924940203767f6d044d076
Muhammad Qamar <146847569+MuhammadQamar99@users.noreply.github.com>
2025-08-02 07:59:51 +0500

    Add files via upload

---
+++ 51_PythonDictionaries.py
@@ -0,0 +1,27 @@
+# Subject : Python Dictionaries 
+# Author : Muhammad qamar
+# Date : 30-July-2025
+
+
+# this_dict = {
+#     'brand' : 'Ford',
+#     'model' : 'Mustang',
+#     'year' : 1964
+# }
+
+# print(type(this_dict))
+# print(len(this_dict))
+
+
+# this_dict1 = {
+#     'name' : 'Muhammad',
+#     'age' : 17,
+#     'color' : False ,
+#     'nickname' : ['qari', 'qamar','hafiz']
+# }
+
+# print(this_dict1)
+
+
+this_dict2 = dict(name = "Muhammad Qamar", age = 17, class1 = '1st Year')
+print(this_dict2)
---
+++ 52_AccessDictionaryItems.py
@@ -0,0 +1,38 @@
+# Subject : Access Dictionary Items in python 
+# Author : Muhammad Qamar
+# Date : 30-July-2025
+
+
+this_dict = {
+    'name' : 'Muhammad qamar',
+    'age' : 17,
+    'gender' : 'M',
+    'religion' : 'Islam'
+}
+
+# print(this_dict['age'])
+# print(this_dict.get('name'))
+
+
+# print(this_dict.keys())
+# x = this_dict.keys()
+# print(x)
+
+# this_dict['color'] = False
+# print(x)
+
+# x = this_dict.values()
+# print(x)
+
+# this_dict['age'] = 18
+# print(x)
+
+# this_dict['color'] = False
+# print(x)
+
+
+# y = this_dict.items()
+# print(y)
+
+if 'grade' in this_dict:
+    print("yes")
