380e5166fa9c8627c988f0563c784d71261c4090
Muhammad Qamar <146847569+MuhammadQamar99@users.noreply.github.com>
2025-08-19 09:17:39 +0500

    Add files via upload

---
+++ 64_PythonLambda.py
@@ -0,0 +1,32 @@
+# Subject : lambda in Python 
+# Author : Muhammad Qamar
+# Date : 17th August 2025
+
+
+# # Example 
+# x = lambda a : a + 10
+# print(x(5))
+
+
+
+
+# # Multiply a and b args
+# y = lambda a,b : a * b
+# print(y(10,10))
+
+
+
+
+# # Summarie the a,b,c
+# z = lambda a,b,c : a + b + c
+# print(z(10,11,12))
+
+
+
+
+# # doubler function 
+# def myfunc(n):
+#     return lambda a : a * n 
+
+# doubler = myfunc(5)
+# print(doubler(10))
---
+++ 65_PythonArray.py
@@ -0,0 +1,11 @@
+# Subject : Array in Python 
+# Author : Muhammad qamar
+# Date : 17th August 2025
+
+
+# # Example
+# cars = ['Ford',"Honda",'Volvo']
+# print(len(cars))
+
+
+
